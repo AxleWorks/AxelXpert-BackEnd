517cb70af2efca8363dfc58ed7c4c4d541cdb767
sahanhansa <sahanhansa.rcg@gmail.com>
2025-10-31 19:14:54 +0530

    feat: Add password reset functionality; include PasswordResetToken entity, repository, and validation in DTO

--- src/main/java/com/login/AxleXpert/auth/DTO/PasswordResetConfirmDTO.java
+++ src/main/java/com/login/AxleXpert/auth/DTO/PasswordResetConfirmDTO.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PasswordResetConfirmDTO {
     
+    @NotBlank(message = "Token is required")
     private String token;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String newPassword;
 }
